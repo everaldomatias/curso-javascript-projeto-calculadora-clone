770def223f81b8ea142f2fc935aeb1993e60f32c
everaldomatias <matiaseveraldo@gmail.com>
2020-02-12 22:17:54 -0300

    Aula 1 e 2.

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,32 @@
+class CalcController {
+
+    constructor() {
+        // Atributos privados
+        this._displayCalc = "0";
+        this._currentDate;
+
+        this.initialize();
+    }
+
+    initialize() {
+
+
+    }
+
+    get displayCalc() {
+        return this._displayCalc;
+    }
+
+    set displayCalc( value ) {
+        this._displayCalc = value;
+    }
+
+    get currentDate() {
+        return this._currentDate;
+    }
+
+    set currentDate( value ) {
+        this._currentDate = value
+    }
+
+}
