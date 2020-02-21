dc1156b25da27a06e5284139e4edf0b92b336410
Everaldo Matias <matiaseveraldo@gmail.com>
2020-02-21 07:56:37 -0300

    Aula C10.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -56,35 +56,45 @@ class CalcController {
 
     }
 
+    setLastOperation(value) {
+
+        this._operation[this._operation.length - 1] = value;
+
+    }
+
     isOperator(value) {
 
-        return ( ['+'. '-', '*', '%', '/'].indexOf(value) > -1 );
+        return ( ['+', '-', '*', '%', '/'].indexOf(value) > -1 );
 
     }
 
     addOperation(value) {
 
-        if(isNaN(this.getLastOperation())) {
+        if (isNaN(this.getLastOperation(value))) {
             // String
-            if (isOperator(value)) {
+            if (this.isOperator(value)) {
                 
                 // Trocar o Operador
-                this._operation[this._operation.length - 1] = value;
+                this.setLastOperation(value);
+
+            } else if(isNaN(value)) {
+
+                console.log(value);
 
             } else {
 
-                console.log(value);
+                // É um número
+                this._operation.push(value);
 
             }
         } else {
             // Number
             let newValue = this.getLastOperation().toString() + value.toString();
-            this._operation.push(newValue);
+            this.setLastOperation(parseInt(newValue));
         }
 
         //this._operation.push(value);
         console.log(this._operation);
-
 
     }
 
@@ -104,23 +114,23 @@ class CalcController {
                 break;
 
             case 'soma':
-
+                this.addOperation('+');
                 break;
 
             case 'subtracao':
-
+                this.addOperation('-');
                 break;
 
             case 'multiplicacao':
-
+                this.addOperation('*');
                 break;
 
             case 'divisao':
-
+                this.addOperation('/');
                 break;
 
             case 'porcento':
-
+                this.addOperation('%');
                 break;
 
             case 'igual':
@@ -128,7 +138,7 @@ class CalcController {
                 break;
 
             case 'ponto':
-
+                this.addOperation('.');
                 break;
 
             case '0':
